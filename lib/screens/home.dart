1abe9508a8c7679646b91161fa798bd72f5ce219
Kelvin Rodrigues <krodrigues@first.pt>
2023-02-07 11:27:31 +0000

    Pausing and replaying without spending more credits on Eleven Labs wortking

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,43 +17,59 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String? chatResponse;
+  bool isPlaying = false;
+  bool hasFinished = false;
+
+  AudioPlayer audioPlayer = AudioPlayer();
+  List<String> userQuestList = [];
+  File? fileAnt;
 
   void getResponse(String userQuest) async {
     APIKey apiKey = APIKey();
     String apiUrl =
         "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM";
 
-    Map<String, String> headers = {
-      'accept': 'audio/mpeg',
-      'xi-api-key': apiKey.getApiKey,
-      'Content-Type': 'application/json',
-    };
+    if (userQuestList.isEmpty) {
+      userQuestList.add(userQuest);
+      Map<String, String> headers = {
+        'accept': 'audio/mpeg',
+        'xi-api-key': apiKey.getApiKey,
+        'Content-Type': 'application/json',
+      };
 
-    Map<String, dynamic> jsonData = {
-      'text': userQuest,
-    };
+      Map<String, dynamic> jsonData = {
+        'text': userQuest,
+      };
 
-    //response = requests.post(apiUrl, headers = headers, json = jsonData);
+      var response = await http.post(Uri.parse(apiUrl),
+          headers: headers, body: json.encode(jsonData));
+      final bytes = response.bodyBytes;
+      final dir = await getTemporaryDirectory();
+      final file = File('${dir.path}/audio.mp3');
+      await file.writeAsBytes(bytes);
+      fileAnt = file;
 
-    var response = await http.post(Uri.parse(apiUrl),
-        headers: headers, body: json.encode(jsonData));
-    AudioPlayer audioPlayer = AudioPlayer();
-    final bytes = response.bodyBytes;
-    final dir = await getTemporaryDirectory();
-    final file = File('${dir.path}/audio.wav');
-    //print(dir.path);
-    await file.writeAsBytes(bytes);
-
-    if (response.statusCode == 200) {
-      //var data = json.decode(utf8.decode(response.bodyBytes));
-      //await audioPlayer.setSourceDeviceFile(file.path);
-      audioPlayer.play(DeviceFileSource(file.path));
-      //audioPlayer.play(audioPlayer.setSourceDeviceFile(file.path))
-    } else {
-      setState(() {
-        //chatResponse = ("Erro: ${response.statusCode}");
+      if (response.statusCode == 200) {
+        audioPlayer.play(DeviceFileSource(file.path));
+        isPlaying = true;
+      } else {
         print("Erro: ${response.statusCode}");
-      });
+      }
+    } else {
+      if (userQuest.contains(userQuest)) {
+        // Checking if the audio has finished
+        audioPlayer.onPlayerComplete.listen((event) {
+          hasFinished = true;
+        });
+
+        if (hasFinished == false) {
+          audioPlayer.resume();
+          isPlaying = true;
+        } else {
+          audioPlayer.play(DeviceFileSource(fileAnt!.path));
+          isPlaying = true;
+        }
+      }
     }
   }
 
@@ -63,8 +79,13 @@ class _HomeState extends State<Home> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            getResponse(
-                'The Surprising Power of Atomic Habits. THE FATE OF British Cycling changed one day in 2003. The organization, which was the governing body for professional cycling in Great Britain, had recently hired Dave Brailsford as its new performance director.');
+            if (isPlaying == false) {
+              getResponse(
+                  'The Surprising Power of Atomic Habits. THE FATE OF British Cycling changed one day in 2003. The organization, which was the governing body for professional cycling in Great Britain, had recently hired Dave Brailsford as its new performance director.');
+            } else {
+              audioPlayer.pause();
+              isPlaying = false;
+            }
           },
           child: const Text('Api'),
         ),
